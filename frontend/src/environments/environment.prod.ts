eb651987f874d4f0b3c28c74e52758dd6c5319ca
Zaidane-E <31802624+Zaidane-E@users.noreply.github.com>
2026-01-25 12:33:14 -0500

    updated frontend deployment env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Replace with your Azure App Service URL after deployment
-  apiUrl: 'https://taskapi-backend.azurewebsites.net/api'
+  apiUrl: 'https://chara-ahgzd3fbage7aybt.canadacentral-01.azurewebsites.net/api'
 };
